f16164726632faa861dcc2c30d0d574669f375ce
leonardo_Wer <name89213126414@gmail.com>
2025-08-06 14:37:53 +0300

    Структура проекта

--- src/App/main.jsx
+++ src/App/main.jsx
@@ -1,6 +1,8 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import '../styles/index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import 's/styles/main.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
