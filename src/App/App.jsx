042bd5cfe1838f2e629ba6676b452b2d5fc3372e
leonardo_Wer <name89213126414@gmail.com>
2025-08-07 11:51:34 +0300

    Горизонтальная прокрутка

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -1,20 +1,68 @@
 import './App.css'
 
-import TopMenu from 's/components/TopMenu/TopMenu.jsx'
+import {useEffect, useRef} from "react";
 
+import { gsap } from 'gsap';
+import { ScrollTrigger } from 'gsap/ScrollTrigger';
+
+gsap.registerPlugin(ScrollTrigger);
+
+import TopMenu from 's/components/TopMenu/TopMenu.jsx'
 import Home from "./sections/Home/Home.jsx"
 import About from "./sections/About/About.jsx"
 import People from "./sections/People/People.jsx"
 
 function App() {
-  return (
+
+    const horizontalScrollWrapper = useRef(null);
+    const horizontalScrollContainer = useRef(null);
+
+    useEffect(() => {
+        if (horizontalScrollContainer.current) {
+
+            function getScrollAmount() {
+                const totalWidth = horizontalScrollContainer.current.offsetWidth;
+                return -(totalWidth - window.innerWidth); // Вся ширина минус разрешение экрана
+            }
+
+            const hs = gsap.to(horizontalScrollContainer.current, {
+                x: () => getScrollAmount(),
+                ease: "none",
+                duration: 3,
+            });
+
+            ScrollTrigger.create({
+                trigger: horizontalScrollWrapper.current,
+                pin: true,
+                start: "top top",
+                end: `+=${getScrollAmount() * -1}`,
+                scrub: 1,
+                invalidateOnRefresh: true,
+                animation: hs
+            })
+
+            // Clean up function (важно!)
+            return () => {
+                ScrollTrigger.getAll().forEach(trigger => trigger.kill());
+            };
+        }
+    }, []);
+
+    return (
     <>
         <TopMenu />
-        <Home />
-        <About />
+
+        <div className="horizontal-scroll-container-wrapper" ref={horizontalScrollWrapper}>
+            <div className="horizontal-scroll-container" ref={horizontalScrollContainer}>
+                <Home />
+                <About />
+            </div>
+        </div>
+
         <People />
+
     </>
-  )
+    )
 }
 
 export default App
