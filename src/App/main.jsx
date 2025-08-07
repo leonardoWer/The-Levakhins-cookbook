7d6affd2fda1c38f1d7422486b29fd18e9b81e81
leonardo_Wer <name89213126414@gmail.com>
2025-08-07 16:54:55 +0300

    Навигация

--- src/App/main.jsx
+++ src/App/main.jsx
@@ -1,16 +1,19 @@
 import 's/styles/main.css'
 
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import {StrictMode} from 'react'
+import {createRoot} from 'react-dom/client'
 import React from 'react';
 import {BrowserRouter} from "react-router-dom";
 
 import App from './App.jsx'
 
+
+const basePath = import.meta.env.BASE_URL || '/The-Levakhins-cookbook';
+
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-      <BrowserRouter basename="/The-Levakhins-cookbook">
-          <App />
-      </BrowserRouter>
-  </StrictMode>,
+    <StrictMode>
+        <BrowserRouter basename={basePath}>
+            <App/>
+        </BrowserRouter>
+    </StrictMode>,
 )
