09b76f3f3e3d42c3cc4d9180a33bb009ec0a0a0d
leonardo_Wer <name89213126414@gmail.com>
2025-08-09 20:48:01 +0300

    Адаптация верхнего меню для телефона

--- src/components/TopMenu/TopMenu.jsx
+++ src/components/TopMenu/TopMenu.jsx
@@ -1,6 +1,6 @@
 import "./TopMenu.css"
 
-import React, {useEffect, useRef} from "react";
+import React, {useEffect, useRef, useState} from "react";
 import {Link, NavLink} from 'react-router-dom'
 
 import logoImg from "/img/logo/logo.png";
@@ -8,9 +8,15 @@ import gsap from "gsap";
 
 function TopMenu() {
 
+    // Кнопка меню
+    const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
+    const toggleMobileMenu = () => {
+        setIsMobileMenuOpen(!isMobileMenuOpen);
+    };
+
+    // Анимация лого
     const logoWrapper = useRef(null);
     const logoSkewAnim = useRef(null);
-
     const handleMouseEnter = () => {
         // Проверяем, что анимация еще не создана
         if (!logoSkewAnim.current) {
@@ -31,42 +37,75 @@ function TopMenu() {
     };
 
     return (
-        <nav className="top-menu">
+        <div className="top-menu-container">
+            <nav className="top-menu">
 
-            <div className="top-menu__left">
-                <div className="logo-container"
-                     ref={logoWrapper}
-                     onMouseEnter={handleMouseEnter}
-                     onMouseLeave={handleMouseLeave}
-                >
-                    <NavLink to="/">
-                        <img className="logo" src={logoImg} alt="logo"/>
-                    </NavLink>
+                <div className="top-menu__left">
+                    <div className="logo-container"
+                         ref={logoWrapper}
+                         onMouseEnter={handleMouseEnter}
+                         onMouseLeave={handleMouseLeave}
+                    >
+                        <NavLink to="/">
+                            <img className="logo" src={logoImg} alt="logo"/>
+                        </NavLink>
+                    </div>
+
+                    <div className="description-container">
+                        <p>
+                            Наши рецепты. Наша история. От нашей кухни к вашей
+                        </p>
+                    </div>
+
                 </div>
 
-                <div className="description-container">
-                    <p className="description-text">
-                        Наши рецепты. Наша история. От нашей кухни к вашей
-                    </p>
+                <div className="top-menu__right">
+                    <ul className="top-menu-list">
+                        <Navigation className={"top-menu-list__item"}/>
+                    </ul>
                 </div>
-            </div>
 
-            <div className="top-menu__right">
-                <ul className="top-menu-list">
-                    <NavLink to="/toc" className="top-menu-list__item">
-                        Оглавление
-                    </NavLink>
-                    <NavLink to="/" className="top-menu-list__item">
-                        О нас
-                    </NavLink>
-                    <NavLink to="/" className="top-menu-list__item">
-                        Контакты
-                    </NavLink>
+                {/* Мобильное меню */}
+                <div className={`mobile-menu-button ${isMobileMenuOpen ? 'active' : ''}`} onClick={toggleMobileMenu}>
+                    <div className="mobile-menu-button__line"></div>
+                    <div className="mobile-menu-button__line"></div>
+                </div>
+
+            </nav>
+
+            {/* Полноэкранное меню */}
+            <div className={`mobile-menu ${isMobileMenuOpen ? 'open' : ''}`}>
+                <ul className="mobile-menu-list">
+                    <Navigation className={"mobile-menu-list__item"} onClick={toggleMobileMenu}/>
+                    <li className="mobile-menu-list__description">
+                        Наши рецепты.<br/>Наша история.<br/>От нашей кухни к вашей
+                    </li>
+
                 </ul>
-            </div>
 
-        </nav>
+
+            </div>
+        </div>
+
     );
+}
+
+function Navigation({className, onClick}) {
+
+    const onClickHandler = onClick ?? "";
+    return (
+        <>
+            <NavLink to="/toc" className={className} onClick={onClickHandler}>
+                Оглавление
+            </NavLink>
+            <NavLink to="/" className={className} onClick={onClickHandler}>
+                О нас
+            </NavLink>
+            <NavLink to="/" className={className} onClick={onClickHandler}>
+                Контакты
+            </NavLink>
+        </>
+    )
 }
 
 export default TopMenu;
