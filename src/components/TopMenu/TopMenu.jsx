94a1f9e9874b668f828e7636b3d3b906aff7c3ad
leonardo_Wer <name89213126414@gmail.com>
2025-08-09 13:18:49 +0300

    Лого

--- src/components/TopMenu/TopMenu.jsx
+++ src/components/TopMenu/TopMenu.jsx
@@ -3,7 +3,7 @@ import "./TopMenu.css"
 import React from "react";
 import { Link, NavLink } from 'react-router-dom'
 
-import bookImg from "/img/book.png";
+import logoImg from "/img/logo/logo.png";
 
 function TopMenu() {
     return (
@@ -12,13 +12,13 @@ function TopMenu() {
             <div className="top-menu__left">
                 <div className="logo-container">
                     <NavLink to="/">
-                        <img className="logo" src={bookImg} alt="logo"/>
+                        <img className="logo" src={logoImg} alt="logo"/>
                     </NavLink>
                 </div>
 
                 <div className="description-container">
                     <p className="description-text">
-                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequatur, cupiditate?
+                        Наши рецепты. Наша история. От нашей кухни к вашей
                     </p>
                 </div>
             </div>
